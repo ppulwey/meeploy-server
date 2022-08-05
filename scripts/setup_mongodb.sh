32f1ac2ca56c3e720c0a908e5459be0957ea8344
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2022-08-05 16:26:44 +0200

    fix db config script

--- scripts/setup_mongodb.sh
+++ scripts/setup_mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Add admin user to mongo db
-echo "db.createUser({user: '$1',pwd: '$2',roles: [{ role: 'dbAdminAnyDatabase', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }, { role: 'userAdminAnyDatabase', db: 'admin' }]})" | mongo admin
+echo "db.createUser({user: '$2',pwd: '$3',roles: [{ role: 'dbAdminAnyDatabase', db: 'admin' }, { role: 'readWriteAnyDatabase', db: 'admin' }, { role: 'userAdminAnyDatabase', db: 'admin' }]})" | mongosh admin
 
 # Overwrite mongo config with auth enabled
-sudo cp -fr database/mongod.conf /etc
+sudo cp -fr $1 /etc
 
 # Restart mongodb
 sudo systemctl restart mongod

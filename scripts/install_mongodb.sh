3f432a8eb5587e2285c61a262ec3e6aabd197d29
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2022-08-05 15:14:36 +0200

    updated mongodb install script

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-cd /etc/apt/sources.list.d
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" >mongodb.list
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 sudo apt update
-yes | sudo apt install mongodb-org
+sudo apt-get install -y mongodb-org
 sudo systemctl enable mongod
 sudo systemctl start mongod
 echo "Please wait 10 seconds ..."
